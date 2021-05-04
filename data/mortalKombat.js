9a104922eef432d518a48665c9049afa9343e057
taylor-tokareff <taylortokareff@gmail.com>
2021-05-04 14:39:18 -0700

    did some stuff I duno lol

---
+++ data/mortalKombat.js
@@ -0,0 +1,151 @@
+// example seed data
+const mortalKombat = [
+  {
+    name: 'Cyrax',
+    species: 'Cyborg',
+    url: 'https://en.wikipedia.org/wiki/Cyrax#/media/File:Cyrax_mk11.png',
+    introduced: 3,
+    isNinja: true,
+    fightingStyle: 'Ninjutsu',
+  },
+
+  {
+    name: 'Sonya Blade',
+    species: 'Human',
+    url: 'https://en.wikipedia.org/wiki/Sonya_Blade#/media/File:Sonya_MK_11.png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Kenpo',
+  },
+
+  {
+    name: 'Sub-Zero',
+    species: 'Human',
+    url: 'https://en.wikipedia.org/wiki/Sub-Zero_(Mortal_Kombat)#/media/File:SubZeroMKXrender.png',
+    introduced: 1,
+    isNinja: true,
+    fightingStyle: 'Shotokan',
+  },
+
+  {
+    name: 'Goro',
+    species: 'Shokan',
+    url: 'https://en.wikipedia.org/wiki/Goro_(Mortal_Kombat)#/media/File:Goro_(Mortal_Kombat).png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Shokan',
+  },
+
+  {
+    name: 'Shang Tsung',
+    species: 'Human',
+    url: 'https://en.wikipedia.org/wiki/Shang_Tsung#/media/File:MK11YoungShangTsung.png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Snake',
+  },
+
+  {
+    name: 'Reptile',
+    species: 'Saurian',
+    url: 'https://en.wikipedia.org/wiki/Reptile_(Mortal_Kombat)#/media/File:ReptileMKXrender.png',
+    introduced: 1,
+    isNinja: true,
+    fightingStyle: 'Hung Gar',
+  },
+
+  {
+    name: 'Sheeva',
+    species: 'Shokan',
+    url: 'https://en.wikipedia.org/wiki/Sheeva#/media/File:Sheeva-removebg-preview.png',
+    introduced: 3,
+    isNinja: false,
+    fightingStyle: 'Kuatan',
+  },
+
+  {
+    name: 'Kabal',
+    species: 'Human',
+    url: 'https://en.wikipedia.org/wiki/Kabal_(Mortal_Kombat)#/media/File:Kabal_MK11.png',
+    introduced: 3,
+    isNinja: false,
+    fightingStyle: 'Goju Ryu',
+  },
+
+  {
+    name: 'Mileena',
+    species: 'Edenian',
+    url: 'https://upload.wikimedia.org/wikipedia/en/a/ac/Mileena_MK11_Design.webp',
+    introduced: 2,
+    isNinja: true,
+    fightingStyle: 'Ying Yeung',
+  },
+
+  {
+    name: 'Johnny Cage',
+    species: 'Human',
+    url: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Johnny_Render_mk_11.png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Jeet Kun Do',
+  },
+  {
+    name: 'Kano',
+    species: 'Human',
+    url: 'https://upload.wikimedia.org/wikipedia/en/8/81/KanoMKXrender.png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Xing Yi',
+  },
+  {
+    name: 'Lui Kane',
+    species: 'Human',
+    url: 'https://upload.wikimedia.org/wikipedia/en/9/92/LiuKangMKXRender.png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Pao Chui',
+  },
+  {
+    name: 'Raiden',
+    species: 'God',
+    url: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Raidenmk11.png/220px-Raidenmk11.png',
+    introduced: 1,
+    isNinja: false,
+    fightingStyle: 'Nan Quan',
+  },
+  {
+    name: 'Scorpion',
+    species: 'Spectre',
+    url: 'https://upload.wikimedia.org/wikipedia/en/6/6c/ScorpionMKXRender.png',
+    introduced: 1,
+    isNinja: true,
+    fightingStyle: 'Hapkido',
+  },
+  {
+    name: 'Kitana',
+    species: 'Edenian',
+    url: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/20/KitanaMK11.png/250px-KitanaMK11.png',
+    introduced: 2,
+    isNinja: false,
+    fightingStyle: 'Eagle Claw',
+  },
+  {
+    name: 'Smoke',
+    species: 'Cyborg',
+    url: 'https://upload.wikimedia.org/wikipedia/en/6/63/Smoke_Mortal_Kombat.png',
+    introduced: 3,
+    isNinja: true,
+    fightingStyle: 'Mi Tzu',
+  },
+  {
+    name: 'Sektor',
+    species: 'Cyborg',
+    url: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Sektor_mk11.png/220px-Sektor_mk11.png',
+    introduced: 3,
+    isNinja: true,
+    fightingStyle: 'Ninjutsu',
+  },
+
+];
+
+export default mortalKombat;
